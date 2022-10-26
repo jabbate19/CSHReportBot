be414a23a14465e83aa74fbc2d94fe392c277a4b
atauln <ataulnoor75@gmail.com>
2022-10-26 14:03:23 -0400

    Checks for text now before sending

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 API_TOKEN = os.environ['API_TOKEN']
 SOCKET_TOKEN = os.environ['SOCKET_TOKEN']
 REPORT_CHANNEL = os.environ['REPORT_CHANNEL']
@@ -12,11 +16,13 @@ app = App(token=API_TOKEN)
 @app.event("message")
 def got_dm(client, event, logger):
     if event['channel_type'] == 'im':
-        client.chat_postMessage(
-            channel=REPORT_CHANNEL,
-            text=event['text']
-        )
+        if 'text' in event:
+            client.chat_postMessage(
+                channel=REPORT_CHANNEL,
+                text=event['text']
+            )
 
 if __name__ == "__main__":
     SocketModeHandler(app, SOCKET_TOKEN).start()
+
 
